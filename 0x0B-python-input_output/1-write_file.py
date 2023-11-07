e5cb3bf6eb543bafa41e7b24115f9303614d2e05
CephaMK <ebenezamk@gmail.com>
2023-11-07 04:54:28 -0800

    json test

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""
+Module 1-number_of_lines
+
+Contains function that returns number of lines in text file
+"""
+
+
 def number_of_lines(filename=""):
-    count = 0
-    with open(filename, encoding="utf-8") as fd:
-        for line in fd:
-            count += 1
-    return count
+    """Return number of lines in text file"""
+    with open(filename, mode="r", encoding="utf-8") as f:
+        lines = 0
+        for line in f:
+            lines += 1
+    return lines
--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
+"""
+Module 2-read_lines
+
+Contains function that reads n lines and prints to stdout
+"""
+
+
 def read_lines(filename="", nb_lines=0):
-    with open(filename, encoding="utf-8") as fd:
-        if (nb_lines == 0):
-            print(fd.read(), end="")
+    """reads n lines and prints to stdout
+    Print:
+        n lines: if n
+        entire file: if n is less than 1 or greater than lines in file
+    """
+    with open(filename, mode="r", encoding="utf-8") as f:
+        if nb_lines <= 0:
+            print(f.read(), end="")
         else:
-            for line in fd:
-                if (nb_lines > 0):
-                    print(line, end="")
-                    nb_lines -= 1
+            while nb_lines:
+                print(f.readline(), end="")
+                nb_lines -= 1
