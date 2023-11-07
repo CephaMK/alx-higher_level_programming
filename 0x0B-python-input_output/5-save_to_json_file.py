b8f9ebc30961d401915958313c8e7ef7ba1c5b7b
CephaMK <ebenezamk@gmail.com>
2023-11-07 05:26:52 -0800

    all tasks done

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-import json
+"""
+Module 7-save_to_json_file
+
+Contains function that writes Python obj to file using JSON represenation
+"""
 
 
-def to_json_string(my_obj):
-    return json.dumps(my_obj)
+def save_to_json_file(my_obj, filename):
+    """Writes Python obj to file using JSON represenation
+    Args:
+        my_obj: python object
+        filename: file
+    """
+    import json
+
+    with open(filename, mode="w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
