947f358f53d7000d43e05f42a32076dfc98bd36b
CephaMK <ebenezamk@gmail.com>
2023-11-08 09:36:28 -0800

    adv test

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
-def add_attribute(obj, a, v):
-    res = getattr(obj, "__doc__", None)
-    if res is None:
-        setattr(obj, a, v)
+"""
+Module 101-add_attribute
+
+Contains function that adds new attribute if possible
+"""
+
+
+def add_attribute(obj, attribute, value):
+    """
+    add attribute to object if possible
+    """
+    if ('__dict__' in dir(obj)):
+        setattr(obj, attribute, value)
     else:
         raise TypeError("can't add new attribute")
